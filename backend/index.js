411b98c5c95854439b8554c185579d66e8196173
Adilsonjvr <adilsonjvr@hotmail.com>
2024-06-03 21:15:30 -0300

    Correção vercel - remoção mongoDB connection option

--- backend/index.js
+++ backend/index.js
@@ -13,15 +13,12 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch((err) => {
-    console.error('MongoDB connection error:', err);
-    process.exit(1);
-});
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => {
+        console.error('MongoDB connection error:', err);
+        process.exit(1);
+    });
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
